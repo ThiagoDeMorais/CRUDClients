06648e06f66e43ca96312229f713c95a8864d7f7
Thigo De Morais <demoraisthiago63@gmail.com>
2022-06-09 15:30:28 -0300

    project created

---
+++ backend/src/test/java/com/thseven/crudclient/CrudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thseven.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
