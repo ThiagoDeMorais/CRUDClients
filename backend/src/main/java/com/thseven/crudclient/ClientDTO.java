3822f5684799e642f7ffdf7e096c67df2c4494ee
Thigo De Morais <demoraisthiago63@gmail.com>
2022-06-09 15:48:27 -0300

    DTO ClientDTO created

---
+++ backend/src/main/java/com/thseven/crudclient/ClientDTO.java
@@ -0,0 +1,82 @@
+package com.thseven.crudclient;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class ClientDTO {
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+	
+	public ClientDTO() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClientDTO other = (ClientDTO) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+}
--- backend/src/main/java/com/thseven/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/thseven/crudclient/resources/ClientResource.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value ="clients")
 public class ClientResource {
-
+	
+	
 }
