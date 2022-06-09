4fb7fc9a454117f17165a293078180e64340137b
Thigo De Morais <demoraisthiago63@gmail.com>
2022-06-09 15:58:23 -0300

    findAll method created in ClientREsources and ClientServices classes

--- backend/src/main/java/com/thseven/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/thseven/crudclient/resources/ClientResource.java
@@ -1,11 +1,23 @@
 package com.thseven.crudclient.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thseven.crudclient.ClientDTO;
+import com.thseven.crudclient.services.ClientService;
 
 @RestController
 @RequestMapping(value ="clients")
 public class ClientResource {
+	ClientService service;
 	
-	
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO> list = new ArrayList<>();
+		list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 }
--- backend/src/main/java/com/thseven/crudclient/services/ClientService.java
+++ backend/src/main/java/com/thseven/crudclient/services/ClientService.java
@@ -1,8 +1,22 @@
 package com.thseven.crudclient.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.thseven.crudclient.ClientDTO;
+import com.thseven.crudclient.entities.Client;
+import com.thseven.crudclient.repositories.ClientRepository;
 
 @Service
 public class ClientService {
+	ClientRepository repository;
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		list.stream().map(client -> new ClientDTO(client));
+		return null;
+	}
 
 }
